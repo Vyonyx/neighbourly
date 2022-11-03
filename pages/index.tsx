7b7617dae7eb67f0b980d8114f629791edccdaa6
personal <shaneel_kumar@live.com>
2022-11-03 14:41:49 +1300

    add random user on click

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,20 @@ import Head from 'next/head' // NextJS Head component to set metadata and title
 // import Image from 'next/image' // NextJS Image component for optimisations
 
 const Home: NextPage = () => {
+  const createUser = async () => {
+    const randomNum = Math.floor(Math.random() * 1000)
+    const res = await fetch('/api/db/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: `User ${randomNum}`,
+        email: `user${randomNum}@gmail.com`
+      })
+    })
+  }
+
   return (
     <div>
       <Head>
@@ -10,6 +24,11 @@ const Home: NextPage = () => {
       </Head>
 
       <h1>Hello World</h1>
+      <button
+        className='btn btn-primary'
+        onClick={createUser}>
+          Add User
+      </button>
     </div>
   )
 }
