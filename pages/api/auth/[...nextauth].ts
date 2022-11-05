55f897794e61a61f9624610ce51f2fa2238a5183
personal <shaneel_kumar@live.com>
2022-11-05 21:58:24 +1300

    add custom auth signin page

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,7 +7,14 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string
     })
-  ]
+  ],
+  pages: {
+    signIn: '/auth/signin',
+    // signOut: '/auth/signout',
+    // error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // (used for check email message)
+    // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  }
 }
 
 export default NextAuth(authOptions)
---
+++ pages/auth/signin.tsx
@@ -0,0 +1,31 @@
+import { getProviders, signIn } from 'next-auth/react'
+
+export default function SignIn({ providers }: any) {
+  return (
+    <>
+      <h1 className='text-3xl'>
+        Sign in with:
+      </h1>
+
+      {Object.values(providers).map((provider: any) => (
+        <div key={provider.name}>
+          <button 
+            onClick={() => signIn(provider.id, {
+              callbackUrl: 'localhost:3000'
+            })}
+            className='btn w-96'
+          >
+            {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+  return {
+    props: { providers },
+  }
+}
