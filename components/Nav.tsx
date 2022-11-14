3599f26c032c977f3565f85cdb28997c3101dad7
personal <shaneel_kumar@live.com>
2022-11-15 12:56:34 +1300

    hide pantry and marketplace nav items if no session

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,52 +13,56 @@ export default function Nav() {
         <Link href='/' className="normal-case text-neutral-d text-4xl hover:cursor-pointer hover:text-primary">N</Link>
       </div>
 
-      <nav className="mr-20 hidden md:inline-flex">
-        <Link
-          href='/'
-          className="">
-            <div className="btn btn-outline border-0 hover:bg-black hover:text-primary">Marketplace</div>
-        </Link>
-        <div className="divider md:divider-horizontal"></div>
-        <Link
-          href='/pantry'
-          className="">
-            <div className="btn btn-outline border-0 hover:bg-black hover:text-primary">Your Pantry</div>
-        </Link>
-      </nav>
 
-      <div className="dropdown dropdown-end md:hidden mr-5">
-        <label tabIndex={0} className='cursor-pointer'>
-          <GiHamburgerMenu className="w-6 h-6 text-neutral-d" />
-        </label>
-        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-          <li><Link href='/'>Marketplace</Link></li>
-          <li><Link href='/pantry'>Your Pantry</Link></li>
-        </ul>
-      </div>
 
       {session && (
-        <div className="flex-none">
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img src={session.user!.image!} alt='profile photo' />
-              </div>
-            </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li className="bg-primary hover:text-primary hover:bg-black">
-                <Link href='/profile'>Profile</Link>
-              </li>
-              <li
-              className="hover:text-primary hover:bg-black"
-              onClick={() => signOut()}>
-                <a>
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </div>
+      <>
+        <nav className="mr-20 hidden md:inline-flex">
+          <Link
+            href='/'
+            className="">
+              <div className="btn btn-outline border-0 hover:bg-black hover:text-primary">Marketplace</div>
+          </Link>
+          <div className="divider md:divider-horizontal"></div>
+          <Link
+            href='/pantry'
+            className="">
+              <div className="btn btn-outline border-0 hover:bg-black hover:text-primary">Your Pantry</div>
+          </Link>
+        </nav>
+
+        <div className="dropdown dropdown-end md:hidden mr-5">
+          <label tabIndex={0} className='cursor-pointer'>
+            <GiHamburgerMenu className="w-6 h-6 text-neutral-d" />
+          </label>
+          <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><Link href='/'>Marketplace</Link></li>
+            <li><Link href='/pantry'>Your Pantry</Link></li>
+          </ul>
+        
         </div>
+          <div className="flex-none">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src={session.user!.image!} alt='profile photo' />
+                </div>
+              </label>
+              <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li className="bg-primary hover:text-primary hover:bg-black">
+                  <Link href='/profile'>Profile</Link>
+                </li>
+                <li
+                className="hover:text-primary hover:bg-black"
+                onClick={() => signOut()}>
+                  <a>
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+      </>
       )}
 
       {!session && (
