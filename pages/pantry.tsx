048c70cb00aaa8185137464a25433609c6f2e9b2
personal <shaneel_kumar@live.com>
2022-11-15 10:41:42 +1300

    complete pantry page styling

--- pages/pantry.tsx
+++ pages/pantry.tsx
@@ -24,25 +24,42 @@ function Pantry() {
 
   
   return (
-    <div className="bg-neutral-l h-full">
+    <div className="flex flex-col lg:flex-row bg-neutral-l h-full">
       <Nav />
 
-      <div className="flex flex-col gap-6 bg-neutral-d pt-32 pb-12 px-12">
+      <div className="flex flex-col gap-6 bg-neutral-d pt-32 pb-12 px-12 lg:h-screen lg:w-96">
         <h1 className="text-5xl text-center text-primary border-b-2 pb-6 border-neutral-l">Pantry</h1>
 
-        <ul className="bg-neutral-l h-full px-10 py-3 divide-y divide-solid divide-neutral flex flex-col shadow-lg">
+        <ul className="bg-neutral-l h-full px-10 py-3 divide-y divide-solid divide-neutral flex flex-col shadow-lg lg:overflow-y-scroll">
           <li className="py-3">Item 1</li>
           <li className="py-3">Item 2</li>
           <li className="py-3">Item 3</li>
           <li className="py-3">Item 4</li>
           <li className="py-3">Item 5</li>
+          <li className="py-3">Item 6</li>
+          <li className="py-3">Item 7</li>
+          <li className="py-3">Item 8</li>
+          <li className="py-3">Item 9</li>
+          <li className="py-3">Item 10</li>
+          <li className="py-3">Item 11</li>
+          <li className="py-3">Item 12</li>
+          <li className="py-3">Item 13</li>
+          <li className="py-3">Item 14</li>
+          <li className="py-3">Item 15</li>
+          <li className="py-3">Item 16</li>
+          <li className="py-3">Item 17</li>
+          <li className="py-3">Item 18</li>
+          <li className="py-3">Item 19</li>
+          <li className="py-3">Item 20</li>
         </ul>
 
         <button className=" mt-6 btn text-neutral-d bg-primary border-0 self-center w-60 hover:bg-black hover:text-primary lg:self-start">Add</button>
       </div>
 
-      <div className="flex flex-col items-center bg-stone-100 h-full py-6 px-12">
-        <h1 className="text-5xl text-neutral-d py-6 border-b-2 border-neutral-d w-full text-center">
+      <div className="flex flex-col items-center bg-stone-100 h-full py-6 px-12 lg:flex-grow lg:pt-32 pb-16 lg:h-screen lg:max-h-screen lg:overflow-y-scroll">
+        <h1
+          className=
+            "text-5xl text-neutral-d py-6 border-b-2 border-neutral-d w-full text-center lg:pt-0 lg:text-start">
           {isEdit ? 'Edit' : 'New'}
         </h1>
 
