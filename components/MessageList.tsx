09246a828392c21d0ecdc24c6bbd73fc278192e7
personal <shaneel_kumar@live.com>
2022-11-16 14:00:46 +1300

    restrict contact component widths

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -5,7 +5,7 @@ function MessageList() {
     <div className="flex flex-col gap-6 w-screen bg-neutral-d pt-32 pb-12 px-12 lg:h-screen lg:w-96">
         <h1 className="text-5xl text-center text-primary border-b-2 pb-6 border-neutral-l">Messages</h1>
 
-        <ul className="h-full px-10 py-3 divide-y divide-solid divide-neutral flex flex-col">
+        <ul className="max-w-lg w-full self-center h-full px-10 py-3 divide-y divide-solid divide-neutral flex flex-col">
           <Contact />
           <Contact />
           <Contact />
