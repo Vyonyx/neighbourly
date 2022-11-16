88ca3b2855208e4779423cbcea25b111218b8afd
personal <shaneel_kumar@live.com>
2022-11-16 16:45:07 +1300

    add message input field

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 const messagesData = [
   {
@@ -16,9 +16,13 @@ const messagesData = [
 function MessageInput() {
   const myID = 1
   const [messages, setMessages] = useState(messagesData)
+
+  const handleSubmit = (evt:React.FormEvent) => {
+    evt.preventDefault()
+  }
   
   return (
-    <div className="p-6 pt-12 lg:pt-32 bg-stone-200 flex-grow">
+    <section className="p-6 pt-12 lg:pt-32 bg-stone-200 flex-grow">
       <ul className="flex flex-col gap-4 max-w-2xl mx-auto">
         {messages.map((item, index) => {
           if (item.senderID === myID) {
@@ -39,7 +43,23 @@ function MessageInput() {
           )
         })}
       </ul>
-    </div>
+
+      <form
+        className="flex flex-col items-center gap-6"
+        onSubmit={handleSubmit}>
+        <textarea
+          className="textarea resize-none w-full mt-6 text-lg py-3 px-6"
+          name=""
+          id=""
+          rows={2}>
+        </textarea>
+        <button
+          className="btn text-neutral-d bg-primary border-0 w-52 hover:bg-black hover:text-primary"
+          type="submit">
+            Send
+        </button>
+      </form>
+    </section>
   )
 }
 export default MessageInput
