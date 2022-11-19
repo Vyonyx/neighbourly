871fe2d99c40c7a7753bdd7a79c7f95aa5d1b8c0
personal <shaneel_kumar@live.com>
2022-11-19 23:06:03 +1300

    move listing data into separate file as mock db

--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -2,40 +2,7 @@
 import Link from "next/link"
 import { useState } from "react"
 
-const listingData = [
-  {
-    id: 1,
-    name: 'Cake!',
-    img: 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1010&q=80',
-    username: 'John Smith',
-    userID: 'abc123',
-    description: 'some generic description about the listing'
-  },
-  {
-    id: 2,
-    name: 'Sourdough?',
-    img: 'https://images.unsplash.com/photo-1563736113551-beda7ee0ebde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-    username: 'Jane Doe',
-    userID: 'abc456',
-    description: 'some generic description about the listing'
-  },
-  {
-    id: 3,
-    name: 'Cake!',
-    img: 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1010&q=80',
-    username: 'John Smith',
-    userID: 'abc123',
-    description: 'some generic description about the listing'
-  },
-  {
-    id: 4,
-    name: 'Sourdough?',
-    img: 'https://images.unsplash.com/photo-1563736113551-beda7ee0ebde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-    username: 'Jane Doe',
-    userID: 'abc456',
-    description: 'some generic description about the listing'
-  },
-]
+import { listingData } from "../lib/listingData"
 
 function Marketplace() {
   const [listings, setListings] = useState<any[]>(listingData)
