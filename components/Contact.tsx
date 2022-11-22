e6b2cab5c5f73a0465be096c6a5f633b3402d6fb
personal <shaneel_kumar@live.com>
2022-11-22 16:45:06 +1300

    link channels collection from db to message list & data to Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,9 @@
-function Contact() {
+function Contact({ channel }:any) {
+  const { receiverName } = channel
+  
   return (
     <li className="py-3 flex items-center justify-between">
-      <span className="text-white">Name</span>
+      <span className="text-white">{receiverName}</span>
       <div className="bg-primary w-10 h-10 rounded-full"></div>
     </li>
   )
