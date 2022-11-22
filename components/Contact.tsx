d52ec03e962aa6a1de97f19012b16a926415115b
personal <shaneel_kumar@live.com>
2022-11-23 11:16:38 +1300

    fix incorrect contact name issue if user is sender

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,7 +5,7 @@ import { updateChannel } from "../slices/channelSlice"
 function Contact({ channel }:any) {
   const dispatch = useDispatch()
   const { data: session } = useSession()
-  const { receiverID, channel:selectedChannel } = channel
+  const { receiverID, receiverName, channel:selectedChannel } = channel
 
   const isReceiver = (receiverID == session?.user!.id) || null
 
@@ -18,7 +18,7 @@ function Contact({ channel }:any) {
       onClick={handleClick}
       className="py-3 flex items-center justify-between cursor-pointer text-white hover:text-primary"
     >
-      <span>{isReceiver ? channel.senderName : session?.user!.name}</span>
+      <span>{isReceiver ? channel.senderName : receiverName}</span>
       <div className="bg-primary w-10 h-10 rounded-full"></div>
     </li>
   )
