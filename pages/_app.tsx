c868acb47c5a3c6ab1a6ebbc9e8b4a9371355a74
personal <shaneel_kumar@live.com>
2022-11-22 23:49:02 +1300

    add redux toolkit store and channel slice

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Session } from 'next-auth'
+import { Provider } from 'react-redux'
+import { store } from '../store'
 import Layout from '../components/layout/Layout'
 
 function MyApp({ 
@@ -10,9 +12,11 @@ function MyApp({
 }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component { ...pageProps} />
-      </Layout>
+      <Provider store={store}>
+        <Layout>
+          <Component { ...pageProps} />
+        </Layout>
+      </Provider>
     </SessionProvider>
   )
 }
