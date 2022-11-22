7a7a879643d8159c690e25dc3d7777fb37a03b44
personal <shaneel_kumar@live.com>
2022-11-23 12:05:38 +1300

    add conditional message if no contacts

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react"
 import Contact from "./Contact"
 import { useSession } from "next-auth/react"
+
 
 function MessageList() {
   const { data:session } = useSession()
@@ -21,10 +23,13 @@ function MessageList() {
   return (
     <div className="flex flex-col gap-6 w-screen bg-neutral-d pt-32 pb-12 px-12 lg:h-screen lg:w-96">
         <h1 className="text-5xl text-center text-primary border-b-2 pb-6 border-neutral-l">Messages</h1>
-
-        <ul className="max-w-lg w-full self-center h-full px-10 py-3 divide-y divide-solid divide-neutral flex flex-col">
-          {channels.map((item:any) => <Contact key={item._id} channel={item} />)}
-        </ul>
+        
+        {(channels.length === 0) && <h1 className="text-md text-white text-center">Contact someone via marketplace</h1>}
+        {(channels.length > 0) && (
+          <ul className="max-w-lg w-full self-center h-full px-10 py-3 divide-y divide-solid divide-neutral flex flex-col">
+            {channels.map((item:any) => <Contact key={item._id} channel={item} />)}
+          </ul>
+        )}
       </div>
   )
 }
