242780c65c8fc57accfde060581e160b1a2b80d4
personal <shaneel_kumar@live.com>
2022-11-23 13:29:46 +1300

    add get listings thunk and api call to listings collection

---
+++ pages/api/db/getListings.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import clientPromise from '../../../lib/mongodb'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const client = await clientPromise
+  const db = client.db('neighbourly')
+
+  if (req.method === 'GET') {
+    try {
+      const listings = await db.collection('listings').find({}).toArray()
+      res.status(200).json(listings)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
--- slices/listingsSlice.ts
+++ slices/listingsSlice.ts
@@ -1,12 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 interface Listings {
   listings: any[];
+  isLoading: boolean;
 }
 
 const initialState:Listings = {
-  listings: []
+  listings: [],
+  isLoading: false
 }
+
+export const getListings = createAsyncThunk(
+  'listings/getListings',
+  async () => {
+    const response = await fetch('../pages/api/db/getListings', {
+      method: 'GET',
+    })
+    const listings = await response.json()
+    return listings
+  }
+)
 
 const listingsSlice = createSlice({
   name: 'listings',
@@ -16,6 +29,18 @@ const listingsSlice = createSlice({
       state.listings.push(action.payload)
     }
   },
+  extraReducers: (builder) => {
+    builder.addCase(getListings.pending, (state) => {
+      state.isLoading = true
+    })
+    builder.addCase(getListings.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.listings = action.payload
+    })
+    builder.addCase(getListings.rejected, (state) => {
+      state.isLoading = false
+    })
+  }
 })
 
 export default listingsSlice.reducer
