e18755c253e0f343ae6d443120fcd5c3396b14d3
personal <shaneel_kumar@live.com>
2022-11-24 13:12:45 +1300

    update dummy data format to match what is required by DB

--- lib/listingData.ts
+++ lib/listingData.ts
@@ -10,27 +10,27 @@ export const listingData = [
     "isGlutenFree": true,
   },
   {
-    id: 2,
-    name: 'Sourdough?',
-    img: 'https://images.unsplash.com/photo-1563736113551-beda7ee0ebde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-    username: 'Jane Doe',
-    userID: 'listing2userid',
-    description: 'some generic description about the listing'
+    "id": 2,
+    "name": "Sourdough?",
+    "img": "https://images.unsplash.com/photo-1563736113551-beda7ee0ebde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "username": "Jane Doe",
+    "userID": "listing2userid",
+    "description": "some generic description about the listing"
   },
   {
-    id: 3,
-    name: 'Cake!',
-    img: 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1010&q=80',
-    username: 'John Smith',
-    userID: 'listing3userid',
-    description: 'some generic description about the listing'
+    "id": 3,
+    "name": "Cake!",
+    "img": "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1010&q=80",
+    "username": "John Smith",
+    "userID": "listing3userid",
+    "description": "some generic description about the listing"
   },
   {
-    id: 4,
-    name: 'Sourdough?',
-    img: 'https://images.unsplash.com/photo-1563736113551-beda7ee0ebde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-    username: 'Jane Doe',
-    userID: 'listing4userid',
-    description: 'some generic description about the listing'
+    "id": 4,
+    "name": "Sourdough?",
+    "img": "https://images.unsplash.com/photo-1563736113551-beda7ee0ebde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    "username": "Jane Doe",
+    "userID": "listing4userid",
+    "description": "some generic description about the listing"
   },
 ]
