b8fd344d9c32d29b77243ba170802cb587132a3e
personal <shaneel_kumar@live.com>
2022-12-24 22:33:01 +1300

    add redirect boilerplate

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,6 +13,10 @@ export const authOptions = {
   ],
   callbacks: {
     async redirect({ url, baseUrl }:any) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
       return baseUrl
     },
     session: async ({ session, token, user }:any) => {
